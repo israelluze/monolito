d3b81c40cc53118181bde73a796ee7165f41d927
Israel Moreira (MTZ - R&D - GOVBR) <Israel.moreira@govbr.com.br>
2025-04-11 17:51:16 -0300

    implementando teste da place-order

--- src/modules/checkout/usecase/place-order/place-order.usecase.spec.ts
+++ src/modules/checkout/usecase/place-order/place-order.usecase.spec.ts
@@ -2,6 +2,54 @@ import { PlaceOrderInputDto } from "./place-order.dto";
 import PlaceOrderUseCase from "./place-order.usecase";
 
 describe("PlaceOrderUseCase", () => {
+
+    describe("validateProducts method", () => {
+       //@ts-expect-error - no params in constructor
+       const placeOrderUseCase = new PlaceOrderUseCase();
+
+       it("should throw an error if no products selected", async () => {
+              const input: PlaceOrderInputDto = {clientId: "0", products:[]};
+              await expect(placeOrderUseCase["validateProducts"](input)).rejects.toThrow(new Error("No products selected"));
+       });
+
+       it("should throw an error when product is out of stock", async ()=> {
+
+            const mockProductFacade = {
+                checkStock: jest.fn(({productId}: {productId: string}) => 
+                Promise.resolve({
+                    productId,
+                    stock: productId === "1" ? 0 : 1,
+                }),
+            ),
+            };
+            //@ts-expect-error - force set productFacade
+            placeOrderUseCase["_productFacade"] = mockProductFacade;
+
+            let input: PlaceOrderInputDto = {clientId: "0", products:[{productId: "1"}]};
+
+            await expect(
+                placeOrderUseCase["validateProducts"](input)
+            ).rejects.toThrow(new Error("Product 1 is out of stock"));
+
+            input = {clientId: "0", products:[{productId: "0"},{productId: "1"}]};
+            await expect(
+                placeOrderUseCase["validateProducts"](input)
+            ).rejects.toThrow(new Error("Product 1 is out of stock"));
+            expect(mockProductFacade.checkStock).toHaveBeenCalledTimes(3);     
+
+            input = {clientId: "0", products:[{productId: "0"},{productId: "1"},{productId:"2"}]};
+            await expect(
+                placeOrderUseCase["validateProducts"](input)
+            ).rejects.toThrow(new Error("Product 1 is out of stock"));
+            expect(mockProductFacade.checkStock).toHaveBeenCalledTimes(5);
+
+
+       });
+        
+
+    });
+
+
     describe("execute method", () => {
 
         it("should throw an error if client not found", async () => {
--- src/modules/checkout/usecase/place-order/place-order.usecase.ts
+++ src/modules/checkout/usecase/place-order/place-order.usecase.ts
@@ -1,13 +1,17 @@
 import UseCaseInterface from "../../../@shared/usecase/use-case.interface";
 import { ClientAdmFacadeInterface } from "../../../client-adm/facade/client-adm.facade.interface";
+import ProductAdmFacadeInterface from "../../../product-adm/facade/product-adm.facade.interface";
 import { PlaceOrderInputDto, PlaceOrderOutputDto } from "./place-order.dto";
 
 export default class PlaceOrderUseCase implements UseCaseInterface {
     private _clientFacade: ClientAdmFacadeInterface;
+    private _productFacade: ProductAdmFacadeInterface; // Assuming this is defined somewhere in the actual code
 
-    constructor(clientFacade: ClientAdmFacadeInterface) {
+    constructor(clientFacade: ClientAdmFacadeInterface,
+                productFacade: ProductAdmFacadeInterface) {
         this._clientFacade = clientFacade;
-    }
+        this._productFacade = productFacade;
+    }    
 
     async execute(input: PlaceOrderInputDto): Promise<PlaceOrderOutputDto> {
         const client = await this._clientFacade.find({id: input.clientId});
@@ -32,6 +36,13 @@ export default class PlaceOrderUseCase implements UseCaseInterface {
     private async validateProducts(input: PlaceOrderInputDto): Promise<void> {
         if (input.products.length === 0) {
             throw new Error("No products selected");
+        }
+
+        for (const product of input.products) {
+            const stock = await this._productFacade.checkStock({productId: product.productId});
+            if (stock.stock <= 0) {
+                throw new Error(`Product ${product.productId} is out of stock`);
+            }
         }
     }
 }
